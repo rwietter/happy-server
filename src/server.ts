91abe9f130f5005258fcd3a030d9a0c13511fd97
rwietter <mauriciobw17@gmail.com>
2020-10-17 13:22:10 -0300

    chore: change port server

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,19 @@
+import './database/dBconnection';
+import 'express-async-errors';
+
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
-import 'express-async-errors';
-import cors from 'cors';
 
 import { errorHandler } from './errors/handler';
-import { Routes } from './routes.ts';
-import './database/dBconnection.ts';
+import { Routes } from './routes';
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(Routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
-app.listen(3333);
-
-/*
-Databases no back-end
-  Driver nativo
-  Query Builder
-  ORM
-
-- Driver native
-  sqlite3.query('SELECT * FROM users')
-- Query Builder
-  knex('users').select('*').where('name')
-- ORM
-  Converte instâncias de uma classe em objeto de relacionamento
-*/
+app.listen(port);
